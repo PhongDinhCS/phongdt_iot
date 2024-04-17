7c55fca59841803b55e561919f2500d28d365d9f
PhongDinhCS <128981139+PhongDinhCS@users.noreply.github.com>
2024-04-17 16:14:57 +0700

    float value Update rs485_thingsboard.py

--- rs485_thingsboard.py
+++ rs485_thingsboard.py
@@ -191,7 +191,7 @@ def serial_read_data(ser):
         print(data_array)
         if len(data_array) >= 7:
             array_size = len(data_array)
-            value = data_array[array_size - 4] * 256 + data_array[array_size - 3]
+            value = float((data_array[array_size - 4] * 256 + data_array[array_size - 3])/100)
             return value
         else:
             return -1
